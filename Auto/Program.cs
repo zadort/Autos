8333578418027571f9692accad99caac4be1dd2e
zadort <zadort@kkszki.hu>
2024-09-24 14:01:09 +0200

    uj auto az adatbazishoz

--- Auto/Program.cs
+++ Auto/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Auto.Model;
 using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
 using MySqlX.XDevAPI.CRUD;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using static Org.BouncyCastle.Asn1.Cmp.Challenge;
 
 namespace Auto
 {
@@ -31,6 +35,28 @@ namespace Auto
             conn.Connection.Close();
         }
 
+        #region 2. feladat
+        //2. feladat
+        public static void addNewCar()
+        {
+            conn.Connection.Open();
+            string brand, type, license;
+            int date;
+            Console.WriteLine("Kérem az autó márkját: ");
+            brand = Console.ReadLine();
+            Console.Write("Kérem az autó típusát: ");
+            type = Console.ReadLine();
+            Console.WriteLine("Kérem az autó motorszámát:  ");
+            license = Console.ReadLine();
+            Console.WriteLine("Kérem az autó gyártási évét: ");
+            date = int.Parse(Console.ReadLine());
+            string sql = $"INSERT INTO `cars`(`Id`, `Brand`, `Type`, `License`, `Date`) VALUES('{brand}', '{type}', '{license}', '{date}')";
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.ExecuteNonQuery();
+            conn.Connection.Close();
+        }
+        #endregion
+
         static void Main(string[] args)
         {
             feltolt();
@@ -38,6 +64,7 @@ namespace Auto
             {
                 Console.WriteLine($"Autó gyártója: {item.Brand}, motorszáma: {item.License}");
             }
+            addNewCar();
 
             Console.ReadLine();
         }
