0daa19c6839b70b61b7a75e7a46d403b68099e76
zadort <zadort@kkszki.hu>
2024-09-24 12:54:44 +0200

    Car class is created.

---
+++ Auto/Model/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto.Model
+{
+    internal class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string License { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
